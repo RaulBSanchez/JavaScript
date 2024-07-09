50f8c312617059d8c3dfb48281c253dcb7e2749a
Raul Bazan <47358801+RaulBSanchez@users.noreply.github.com>
2024-07-09 15:57:10 -0400

    learning JS

---
+++ BlackJack/index.js
@@ -0,0 +1,21 @@
+let firstCard = 10 
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = " "
+
+if (sum <= 20){
+    message = "Do you want to draw a new card?"
+}
+else if(sum === 21){
+    message = "Woohoo! You've got blackjack"
+    hasBlackJack = true
+}
+else{
+    message = "You're out of the game"
+    isAlive = false
+}
+
+
+console.log(message)
