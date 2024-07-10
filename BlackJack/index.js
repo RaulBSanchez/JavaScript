6f2fd9b48e55c034e4c73031cd0f699b70724612
Raul Bazan <47358801+RaulBSanchez@users.noreply.github.com>
2024-07-10 17:56:00 -0400

    working on my projects

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,21 +1,46 @@
 let firstCard = 10 
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = " "
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum <= 20){
-    message = "Do you want to draw a new card?"
-}
-else if(sum === 21){
-    message = "Woohoo! You've got blackjack"
-    hasBlackJack = true
-}
-else{
-    message = "You're out of the game"
-    isAlive = false
-}
+let cards = [firstCard, secondCard]
 
 
-console.log(message)
+function startGame(){
+    renderGame()
+}
+
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards
+    
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You got Blackjack! "
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! "
+        isAlive = false
+    }
+    
+    messageEl.innerText = message
+}
+
+function newCard() {
+    let card = 3
+
+    sum += card
+    cards.push(card)
+    renderGame()
+}
