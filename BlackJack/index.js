0fa7f6ad20045217af19fd6c09694cac108083f6
Raul Bazan <47358801+RaulBSanchez@users.noreply.github.com>
2024-07-11 16:09:01 -0400

    chugging along

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,23 +1,42 @@
-let firstCard = 10 
-let secondCard = 4
-let sum = firstCard + secondCard
+
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = " "
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-let cards = [firstCard, secondCard]
+let cards = []
 
+//console.log(getRandomCard())
+function getRandomCard(){
+    
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    if (randomNumber > 10){
+        return 10
+    }
+    else if (randomNumber ===1){
+        return 11
+    }
+    else {
+        return randomNumber
+    }
+}
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards
+    cardsEl.textContent = "Cards: " 
     
     for (let i = 0; i < cards.length; i++){
         cardsEl.textContent += cards[i] + " "
@@ -38,7 +57,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 3
+    let card = getRandomCard()
 
     sum += card
     cards.push(card)
